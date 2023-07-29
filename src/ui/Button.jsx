e081868901013fc2a5641d799227b948347ec4f1
mehar sulaiman <dr.love9026@gmail.com>
2023-07-29 22:23:21 +0500

    order page styling

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 export default function Button({ children, disabled, to, type }) {
   const base =
-    "inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 active:translate-y-[1px] disabled:cursor-not-allowed";
+    "inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 active:translate-y-[1px] disabled:cursor-not-allowed";
 
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
     small: base + "  px-4 py-2 md:px-4 md:py-2.5 text-xs",
     secondary:
-      "inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 active:translate-y-[1px] disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5 transition-colors duration-300",
+      "inline-block rounded-full border-2 text-sm border-stone-300 font-semibold uppercase tracking-wide text-stone-400 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 active:translate-y-[1px] disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5 transition-colors duration-300",
   };
 
   if (to)
