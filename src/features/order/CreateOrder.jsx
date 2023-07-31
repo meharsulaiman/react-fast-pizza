4229081e40b267b748dfba80b18ccd819a320b68
mehar sulaiman <dr.love9026@gmail.com>
2023-07-31 09:13:14 +0500

    userslice have done

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -32,6 +33,7 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const { username } = useSelector((state) => state.user);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
   const formErrors = useActionData();
@@ -51,6 +53,7 @@ function CreateOrder() {
               name="customer"
               required
               className="input w-full"
+              defaultValue={username}
             />
           </div>
         </div>
